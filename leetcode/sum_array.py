02703fac3bc7bdb5f769c0a5887c7e176d699a35
nkchangliu <nkchangliu@gmail.com>
2018-07-26 18:33:59 -0400

    fixed DP

--- leetcode/sum_array.py
+++ leetcode/sum_array.py
@@ -20,18 +20,37 @@ def _sum_array(lst, k, end, groups, cache):
 
 
 
-def sum_array_dp(lst, k):
-    res = [[(0, []) for i in range(len(lst))] for j in range(4)]
+def sum_array_dp(lst, sum_lst, k):
+    res = [[0 for i in range(len(lst))] for j in range(4)]
 
     for j  in range(1, len(lst)):
         for i in range(1, 4):
-            not_include_val, not_include_lst = res[i][j - 1]
             res[i][j] = res[i][j - 1]
             if j - k + 1 >= 0:
-                include_val, include_lst = res[i - 1][j - k]
-                if include_val + sum(lst[j - k + 1: j + 1]) > not_include_val:
-                    new_lst = include_lst + [j - k + 1]
-                    res[i][j] = (include_val + sum(lst[j - k + 1: j + 1]), new_lst)
-    return res[3][len(lst) - 1][1]
+                if res[i - 1][j - k] + sum_lst[j + 1] - sum_lst[j - k + 1] > res[i][j - 1]:
+                    res[i][j] = res[i - 1][j - k] + sum_lst[j + 1] - sum_lst[j - k + 1]
+    return res
 
-print(sum_array_dp([1,2,1,2,6,7,5,1], 2))
+def prefix_sum(lst):
+    sum_lst = [0]
+    prefix_s = 0
+    for num in lst:
+        prefix_s +=  num
+        sum_lst.append(prefix_s)
+    return sum_lst
+
+def get_path(lst, k):
+    sum_lst = prefix_sum(lst)
+    res = sum_array_dp(lst, sum_lst, k)
+    path = []
+    i, j = 3, len(lst) - 1
+    while i > 0 and j > 0:
+        if res[i - 1][j - k ] + sum_lst[j + 1] - sum_lst[j - k + 1] > res[i][j - 1]:
+            path.append(j - k + 1)
+            j = j - k
+            i -= 1
+        else:
+            j -= 1
+    return path[::-1]
+
+print(get_path([1,2,1,2,6,7,5,1], 2))
